d611ab3dd41a6807d187cf18be7ae921b64511a3
Yazmin Perez <zoeperez333@gmail.com>
2022-02-13 12:07:12 -0800

    create three place objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
-function Places(location, landmark, date) {
+function Places(location, landmark, season) {
   this.location = location;
   this.landmark = landmark;
-  this.date = date;
+  this.season = season;
 }
+
+let seattle = new Places('Washington State', 'Mt Rainer', 'summer');
+let portland = new Places('Oregon', 'Mt Hood', 'winter');
+let moab = new Places('Utah', 'Arches', 'spring');
 
 
 
